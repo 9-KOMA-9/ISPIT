6831cecd3a23d15229aa3e920741807c95f4e7fc
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-11-15 09:26:53 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provjeraznanja5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("unesite koliko brojeva zelite upisati.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            double[] polje;
+            for(int i = 0; i < x; i++)
+            {
+                Console.WriteLine("Unesite " + x + " broj");
+                polje[x] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+    }
+}
